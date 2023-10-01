8c0a58a9a3059988f13a9f680bd9a4e636d64443
Flaa64 <cerquettiflavia@gmail.com>
2023-10-01 12:06:21 -0300

    feat: add archivos de entrega

---
+++ archivos_de_entrega/view.sql
@@ -0,0 +1,31 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `trailerflix`.`new_view` AS
+    SELECT 
+        `t`.`id` AS `id`,
+        `t`.`poster` AS `poster`,
+        `t`.`titulo` AS `titulo`,
+        `c`.`categoria` AS `categoria`,
+        GROUP_CONCAT(DISTINCT `g`.`genero`
+            SEPARATOR ', ') AS `genero`,
+        `t`.`resumen` AS `resumen`,
+        `t`.`temporadas` AS `temporadas`,
+        GROUP_CONCAT(DISTINCT `a`.`actor`
+            SEPARATOR ', ') AS `actores`,
+        `t`.`trailer` AS `trailer`
+    FROM
+        (((((`trailerflix`.`titulos` `t`
+        JOIN `trailerflix`.`categorias` `c`)
+        JOIN `trailerflix`.`generos` `g`)
+        JOIN `trailerflix`.`actores` `a`)
+        JOIN `trailerflix`.`genero_titulo` `gt`)
+        JOIN `trailerflix`.`reparto` `r`)
+    WHERE
+        ((`t`.`id` = `gt`.`idTitulo`)
+            AND (`t`.`id` = `r`.`idTitulo`)
+            AND (`t`.`categoria` = `c`.`id`)
+            AND (`gt`.`idGenero` = `g`.`id`)
+            AND (`a`.`id` = `r`.`idActor`))
+    GROUP BY `t`.`id`
